0887f48950065766658911cc993d3281d627b5fd
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-11 21:49:34 +0600

    banner added

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,34 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { FreeMode, Pagination } from 'swiper/modules';
+import slider1 from '../../../assets/home/slide1.jpg'
+import slider2 from '../../../assets/home/slide2.jpg'
+import slider3 from '../../../assets/home/slide3.jpg'
+import slider4 from '../../../assets/home/slide4.jpg'
+import slider5 from '../../../assets/home/slide5.jpg'
+
+const Category = () => {
+    return (
+        <div>
+            <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src={slider1} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider2} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider3} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider4} alt="" /></SwiperSlide>
+        <SwiperSlide><img src={slider5} alt="" /></SwiperSlide>
+      </Swiper>
+        </div>
+    );
+};
+
+export default Category;
--- src/pages/Sheard/Navbar/Navbar.jsx
+++ src/pages/Sheard/Navbar/Navbar.jsx
@@ -1,34 +1,35 @@
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
 
     const navLink = <>
-       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <navLink to='/'>Home</navLink>
-                            <navLink to='/contactUs'>Contact Us</navLink>
-                            <navLink to='/dashBoard'>DashBoard</navLink>
-                            <navLink to='/ourMenu'>Our Menu</navLink>
-                            
-                        </ul>
+                <li><NavLink to='/'>HOME</NavLink></li>
+                <li><NavLink to='/contactUs'>CONTACT US</NavLink></li>
+                <li><NavLink to='/dashBoard'>DASHBOARD</NavLink></li>
+                <li><NavLink to='/ourMenu'>OUR MENU</NavLink></li>
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar static bg-base-100  rounded-lg">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        {navLink}
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navLink}
+                        </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">BISTRO BOSS<br/>Restaurant</a>
+                    <a className=" normal-case text-xl font-semibold">BISTRO BOSS</a>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    {navLink}
-                </div>
-                <div className="navbar-end">
+                <div className="navbar-end hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {navLink}
+                    </ul>
                     <a className="btn">Button</a>
                 </div>
+                
             </div>
         </div>
     );
