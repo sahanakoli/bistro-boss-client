03a1c42e02effa0ddb69808e6f3c57e9faf37cf3
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-17 23:11:04 +0600

    save user if he doesn't exist in the database

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { FcGoogle } from "react-icons/fc";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
 
     const { googleSignIn } = useContext(AuthContext);
-
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
 
     const handleGoogle = () =>{
         googleSignIn()
         .then(result =>{
             console.log(result.user)
+            const userInfo = {
+                email: result.user?.email,
+                name: result.user?.displayName
+            }
+            axiosPublic.post('/users', userInfo)
+            .then(res =>{
+                console.log(res.data);
+                navigate('/');
+            })
         })
         .catch(error =>{
             console.error(error)
