2ca295bb7ea1fb231e479604c517f6e30295887a
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-17 19:39:28 +0600

    display cart items on the cart page

--- src/pages/DashBoard/Cart/Cart.jsx
+++ src/pages/DashBoard/Cart/Cart.jsx
@@ -1,17 +1,22 @@
+import { FaTrash } from 'react-icons/fa6';
 import useCart from '../../../hooks/useCart';
 
 const Cart = () => {
     const [cart] = useCart();
-    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+    const handleDelete = () =>{
+
+    }
     return (
         <div>
-            <div className='flex justify-evenly'>
+            <div className='flex justify-evenly mb-10'>
                 <h2 className='text-4xl'>Items: {cart.length}</h2>
                 <h2 className='text-4xl'>Total Price: {totalPrice}</h2>
                 <button className='btn bg-[#D1A054]'>Pay</button>
             </div>
             <div className="overflow-x-auto">
-                <table className="table">
+                <table className="table w-full">
                     {/* head */}
                     <thead>
                         <tr>
@@ -26,9 +31,9 @@ const Cart = () => {
                     </thead>
                     <tbody>
                         {
-                            cart.map(item => <tr key={item._id}>
+                            cart.map((item) => <tr key={item._id}>
                                 <th>
-                                    
+                                 
                                 </th>
                                 <td>
                                     <div className="flex items-center gap-3">
@@ -37,18 +42,14 @@ const Cart = () => {
                                                 <img src={item.image} />
                                             </div>
                                         </div>
-                                        <div>
-                                            <div className="font-bold">Hart Hagerty</div>
-                                            <div className="text-sm opacity-50">United States</div>
-                                        </div>
                                     </div>
                                 </td>
                                 <td>
                                     {item.name}
                                 </td>
-                                <td>{item.price}</td>
+                                <td>${item.price}</td>
                                 <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
+                                    <button onClick={() => handleDelete(item._id)} className="btn btn-ghost btn-xs"><FaTrash className=' text-red-600'></FaTrash></button>
                                 </th>
                             </tr>)
                         }
