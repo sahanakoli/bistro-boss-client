25d94ac62737769dacf596c9eb16014a750f2993
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-22 10:57:32 +0600

    display admin stats

--- src/pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,16 +1,66 @@
+import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../hooks/useAuth";
-
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { FaBook, FaDollarSign, FaUsers } from "react-icons/fa";
+import { CiMemoPad } from "react-icons/ci";
 
 const AdminHome = () => {
     const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: stats = {} } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/admin-stats');
+            return res.data;
+        }
+    });
     return (
         <div>
-           <h2 className=" text-3xl">
-            <span>Hi, Welcome </span>
-            {
-                user?.displayName ? user.displayName : 'Back'
-            }
-           </h2> 
+            <h2 className=" text-3xl">
+                <span>Hi, Welcome </span>
+                {
+                    user?.displayName ? user.displayName : 'Back'
+                }
+            </h2>
+            <div className="stats shadow">
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    <FaDollarSign className=" text-3xl" />
+                    </div>
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value">${stats.revenue}</div>
+                    <div className="stat-desc">Jan 1st - Feb 1st</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaUsers className=" text-3xl" />
+                    </div>
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value">{stats.users}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                        <FaBook className=" text-2xl" />
+                    </div>
+                    <div className="stat-title">MenuItem</div>
+                    <div className="stat-value">{stats.menuItems}</div>
+                    <div className="stat-desc">↗︎ 400 (22%)</div>
+                </div>
+
+                <div className="stat">
+                    <div className="stat-figure text-secondary">
+                    <CiMemoPad className=" text-3xl" />  
+                    </div>
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value">{stats.orders}</div>
+                    <div className="stat-desc">↘︎ 90 (14%)</div>
+                </div>
+
+            </div>
         </div>
     );
 };
